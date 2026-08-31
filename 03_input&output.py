d1cbcc08ff516c676abd2f3405b6d0d36878c7c1
salmanmasroor <salmanmasroor07@gmail.com>
2026-08-31 13:31:35 +0500

    input and output

---
+++ 03_input&output.py
@@ -0,0 +1,23 @@
+"""
+Definition: 
+    - Input means getting data into your Python program.
+    - Output means sending/displaying data from your Python program.
+"""
+
+#output
+print("Python programming")
+
+#seperator
+print("Python", "Programming",sep="-")
+
+#Printing multiple values
+name = "Hamza"
+age = 20
+print(name,age)
+
+#input
+name = input("Enter the Name: ")
+age = int(input("Enter the age: "))
+
+print(name,type(name),sep="--")
+print(age,type(age), sep="--")
